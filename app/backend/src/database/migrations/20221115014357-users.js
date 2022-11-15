1f0028180d5165c8ebc52e355b1c3f09e49cfc07
Pedro Pereira <pedro.2108@hotmail.com>
2022-11-14 23:08:43 -0300

    feat: Model and migration for Users implemented

---
+++ app/backend/src/database/migrations/20221115014357-users.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('users', { 
+      id: {
+        type: Sequelize.UUID,
+        primaryKey: true,
+        defaultValue: Sequelize.UUIDV4
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      accountId: {
+        type: Sequelize.UUID,
+        allowNull: false,
+      }
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('users');
+  }
+};
---
+++ app/backend/src/database/models/Users.ts
@@ -0,0 +1,51 @@
+import { STRING, UUIDV4 } from 'sequelize';
+import { UUID } from 'sequelize';
+import { Model, Optional } from 'sequelize'
+import db from './';
+import Accounts from './Accounts';
+
+type UsersAttributes = {
+  id: string
+  username: string
+  password: string
+  accountId: string
+}
+
+type UsersCreationAttributes = Optional<UsersAttributes, 'id'>
+
+export default class Users extends Model<UsersAttributes, UsersCreationAttributes> {
+  declare id: string;
+  declare username: string;
+  declare password: string;
+  declare accountId: string;
+}
+
+Users.init(
+  {
+    id: {
+      type: UUID,
+      primaryKey: true,
+      defaultValue: UUIDV4
+    },
+    username: {
+      type: STRING,
+      allowNull: false
+    },
+    password: {
+      type: STRING,
+      allowNull: false
+    },
+    accountId: {
+      type: UUID,
+      allowNull: false,
+    }
+  },
+  {
+    sequelize: db,
+    modelName: 'users',
+    timestamps: false,
+    underscored: true
+  }
+);
+
+Users.belongsTo(Accounts, { foreignKey: 'id', as: 'accountId' });
