c6c3ee32489a42c5491462f5d700ffaf32bc881e
Pedro Pereira <pedro.2108@hotmail.com>
2022-11-15 20:11:56 -0300

    feat: Token validation method implemented

--- app/backend/src/services/utils/Token.ts
+++ app/backend/src/services/utils/Token.ts
@@ -6,7 +6,18 @@ const JWT_SECRET = process.env.JWT_SECRET || 'ng_Cash';
 const JWT_OPTIONS = { expiresIn: '1d' };
 
 export default class Token {
-  public static generate(email: string) {
-    return jwt.sign({ email }, JWT_SECRET, JWT_OPTIONS);
+  public static generate(username: string) {
+    return jwt.sign({ username }, JWT_SECRET, JWT_OPTIONS);
+  }
+
+  public static validate(token: string) {
+    try {
+      const payload = jwt.verify(token, JWT_SECRET);
+
+      return payload;
+    } catch (err) {
+      return null;
+    }
+
   }
 }
