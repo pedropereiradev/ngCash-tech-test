6b2b55d84be500f8e44b8f0e034f3a0b776fbc06
Pedro Pereira <pedro.2108@hotmail.com>
2022-11-16 16:56:52 -0300

    feat: User creation API implemented

--- app/frontend/src/services/api.ts
+++ app/frontend/src/services/api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const LOGIN_ENDPOINT = process.env.LOGIN_ENDPOINT || 'http://localhost:3001/login';
+const USER_REGISTER_ENDPOINT = process.env.USER_REGISTER_ENDPOINT || 'http://localhost:3001/user';
 
 type UserType = {
   username: string
@@ -10,6 +11,16 @@ type UserType = {
 export async function login({ username, password }: UserType) {
   try {
     const { data } = await axios.post(LOGIN_ENDPOINT, { username, password });
+
+    return data;
+  } catch (error) {
+    return error;
+  }
+}
+
+export async function userRegister(registerParams: UserType) {
+  try {
+    const { data } = await axios.post(USER_REGISTER_ENDPOINT, registerParams);
 
     return data;
   } catch (error) {
