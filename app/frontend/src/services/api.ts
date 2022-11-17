b2252c75a72144116bc1cf2cdd3333abd8d61e7c
Pedro Pereira <pedro.2108@hotmail.com>
2022-11-16 23:54:50 -0300

    feat: API functions to transactions, users and accounts implemented

--- app/frontend/src/services/api.ts
+++ app/frontend/src/services/api.ts
@@ -1,14 +1,24 @@
 import axios from 'axios';
+import { getToken } from './userLocalStorage';
 
 const LOGIN_ENDPOINT = process.env.LOGIN_ENDPOINT || 'http://localhost:3001/login';
-const USER_REGISTER_ENDPOINT = process.env.USER_REGISTER_ENDPOINT || 'http://localhost:3001/user';
+const USER_ENDPOINT = process.env.USER_ENDPOINT || 'http://localhost:3001/user';
+const USER_BALANCE_ENDPOINT = process.env.USER_BALANCE_ENDPOINT || 'http://localhost:3001/account/balance';
+const USERS_ACCOUNTS_ENDPOINT = process.env.USERS_ACCOUNTS_ENDPOINT || 'http://localhost:3001/user/all'; 
+const TRANSACTION_ENDPOINT = process.env.TRANSACTION_ENDPOINT || 'http://localhost:3001/transaction';
 
-type UserType = {
+type UserData = {
   username: string
   password: string
 };
 
-export async function login({ username, password }: UserType) {
+type TransactionData = {
+  originAccount: string
+  destinationAccount: string
+  value: number
+};
+
+export async function login({ username, password }: UserData) {
   try {
     const { data } = await axios.post(LOGIN_ENDPOINT, { username, password });
 
@@ -18,12 +28,74 @@ export async function login({ username, password }: UserType) {
   }
 }
 
-export async function userRegister(registerParams: UserType) {
+export async function userRegister(registerParams: UserData) {
   try {
-    const { data } = await axios.post(USER_REGISTER_ENDPOINT, registerParams);
+    const { data } = await axios.post(USER_ENDPOINT, registerParams);
 
     return data;
   } catch (error) {
     return error;
   }
 }
+
+export async function getBalance() {
+  try {
+    const { data } = await axios.get(USER_BALANCE_ENDPOINT, {
+      headers: { 'authorization': getToken() },
+    });
+
+    return data;
+  } catch (error) {
+    return error;
+  }
+}
+
+export async function getUserAccountInfo() {
+  try {
+    const { data } = await axios.get(USER_ENDPOINT, {
+      headers: { 'authorization': getToken() },
+    });
+
+    return data;
+  } catch (error) {
+    return error;
+  }
+}
+
+export async function getAllUsers() {
+  try {
+    const { data } = await axios.get(USERS_ACCOUNTS_ENDPOINT, {
+      headers: { 'authorization': getToken() },
+    });
+
+    return data;
+  } catch (error) {
+    return error;
+  }
+}
+
+export async function createTransaction(transactionParams:TransactionData) {
+  try {
+    const { data } = await axios.post(TRANSACTION_ENDPOINT, transactionParams, {
+      headers: { 'authorization': getToken() },
+    });
+
+    return data;
+  } catch (error) {
+    return error;
+  }
+}
+
+export async function getAllTransactions() {
+  try {
+    const { data } = await axios.get(TRANSACTION_ENDPOINT, {
+      headers: { 'authorization': getToken() },
+    });
+
+    return data;
+  } catch (error) {
+    return error;
+  }
+}
+
+// export async function getFilteredTransactions() {}
