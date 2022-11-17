77d63852888b4b279f3eb52d07856ce9ab8d238a
Pedro Pereira <pedro.2108@hotmail.com>
2022-11-17 01:39:35 -0300

    feat: Initial page implemented

--- app/frontend/src/pages/Initial.tsx
+++ app/frontend/src/pages/Initial.tsx
@@ -1,8 +1,28 @@
-import React from 'react';
+import * as React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { getUserAccountInfo } from '../services/api';
 
 function Initial() {
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    async function validateToken() {
+      const result = await getUserAccountInfo();
+
+      if (result.username) navigate('/dashboard');
+    }
+
+    validateToken();
+  }, []);
+
   return (
-    <div>Initial</div>
+    <>
+      <h1>Welcome to NGCash</h1>
+      <section>
+        <button type='button' onClick={() => navigate('/login')}>Login</button>
+        <button type='button' onClick={() => navigate('/register')}>Register</button>
+      </section>
+    </>
   );
 }
 
