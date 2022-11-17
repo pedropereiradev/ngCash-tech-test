fb79e772d1b7b7ff68785dec0e6b66c17baf449e
Pedro Pereira <pedro.2108@hotmail.com>
2022-11-16 23:55:15 -0300

    refact: User localstorage refactored

--- app/frontend/src/services/userLocalStorage.ts
+++ app/frontend/src/services/userLocalStorage.ts
@@ -1,29 +1,14 @@
-const USER_KEY = 'user';
+const TOKEN_KEY = 'token';
 
-type UserType = {
-  username: string,
-  token: string,
-  balance: number
-};
 
-const INITIAL_VALUE = {
-  username: '',
-  token: '',
-  balance: 0,
-};
-
-export const getUser = () => {
-  if (!localStorage.getItem(USER_KEY)) {
-    localStorage.setItem(USER_KEY, JSON.stringify(INITIAL_VALUE));
+export const getToken = (): string => {
+  if (!localStorage.getItem(TOKEN_KEY)) {
+    localStorage.setItem(TOKEN_KEY, JSON.stringify(''));
   }
 
-  return JSON.parse(localStorage.getItem(USER_KEY) || '');
+  return JSON.parse(localStorage.getItem(TOKEN_KEY) || '');
 };
 
-export const saveUser = (user: UserType) => localStorage.setItem(USER_KEY, JSON.stringify(user));
+export const saveToken = (token: string) => localStorage.setItem(TOKEN_KEY, JSON.stringify(token));
 
-export const getToken = () => {
-  const { token } = getUser();
-
-  return token;
-};
+export const logout = () => localStorage.removeItem(TOKEN_KEY);
