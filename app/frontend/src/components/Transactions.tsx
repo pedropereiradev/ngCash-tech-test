ef0694095836f2db3a67de905ddca1ec6e5a847e
Pedro Pereira <pedro.2108@hotmail.com>
2022-11-18 11:24:18 -0300

    style: Dashboard transactions table initial style implemented

--- app/frontend/src/components/Transactions.tsx
+++ app/frontend/src/components/Transactions.tsx
@@ -1,8 +1,11 @@
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
 import React from 'react';
 import { Calendar } from 'react-calendar';
 import { AppContext } from '../context/AppContext';
 import { getAllTransactions, getFilteredTransactions } from '../services/api';
 import { IAccount, ITransaction } from '../services/interfaces';
+import NorthIcon from '@mui/icons-material/North';
+import SouthIcon from '@mui/icons-material/South';
 
 type TargetFiltersData = {
   target: {
@@ -55,9 +58,16 @@ function Transactions() {
     }));
   };
 
+  const formatDate = (inputDate: string) => {
+    const date = inputDate.split('-');
+    
+    return `${date[2]}/${date[1]}/${date[0]}`;
+  };
+
+
   return (
     <section>
-      <h2>Transactions</h2>
+      <h2>Transações realizadas</h2>
       <section>
         <select value={order.orderBy} onChange={handleFiltersChange} name='orderBy' placeholder='Order by'>
           <option value=''>All</option>
@@ -75,28 +85,35 @@ function Transactions() {
         }
         <button type='button' onClick={clearFilters}>Clear filters</button>
       </section>
-      <table>
-        <thead>
-          <tr>
-            <th>Item</th>
-            <th>Value</th>
-            <th>Account</th>
-            <th>Date</th>
-            <th>Type</th>
-          </tr>
-        </thead>
-        <tbody>
-          {accounts.length && transactions.map((transaction: ITransaction, index) => (
-            <tr>
-              <td>{index + 1}</td>
-              <td>{Number(transaction.value).toFixed(2).replace('.', ',')}</td>
-              <td>{transaction.creditedAccountId === user.accountId ? getUserFromAccount(transaction.debitedAccountId) : getUserFromAccount(transaction.creditedAccountId)}</td>
-              <td>{transaction.transactionDate}</td>
-              <td>{transaction.creditedAccountId === user.accountId ? 'Sent' : 'received'}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <Paper elevation={4}>
+        <TableContainer>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Item</TableCell>
+              <TableCell>Valor</TableCell>
+              <TableCell>Conta</TableCell>
+              <TableCell>Data</TableCell>
+              <TableCell>Recebido/Enviado</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {accounts.length && transactions.map((transaction: ITransaction, index) => (
+              <TableRow>
+                <TableCell>{index + 1}</TableCell>
+                <TableCell>{`R$ ${Number(transaction.value).toFixed(2).replace('.', ',')}`}</TableCell>
+                <TableCell>{transaction.creditedAccountId === user.accountId ? getUserFromAccount(transaction.debitedAccountId) : getUserFromAccount(transaction.creditedAccountId)}</TableCell>
+                <TableCell>{formatDate(transaction.transactionDate)}</TableCell>
+                <TableCell>
+                  {transaction.creditedAccountId === user.accountId ? 'Recebido' : 'Enviado'}
+                  {transaction.creditedAccountId === user.accountId ? <SouthIcon color='success' /> : <NorthIcon color='error' /> }
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      </Paper>
     </section>
   );
 }
