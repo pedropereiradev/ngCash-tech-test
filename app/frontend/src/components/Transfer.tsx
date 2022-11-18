66648ebab76af1237321a68a3014b6ce5c75507b
Pedro Pereira <pedro.2108@hotmail.com>
2022-11-18 17:55:58 -0300

    style: Transfer component styled

--- app/frontend/src/components/Transfer.tsx
+++ app/frontend/src/components/Transfer.tsx
@@ -1,3 +1,4 @@
+import { Box, Button, FormControl, FormHelperText, InputAdornment, InputLabel, MenuItem, OutlinedInput, Select, Stack, Typography } from '@mui/material';
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import { AppContext } from '../context/AppContext';
@@ -34,28 +35,92 @@ function Transfer() {
     reset();
   });
   return (
-    <section>
-      <h2>Make Transaction</h2>
-      <form>
-        <select {...register('debitedAccountId', { required: 'Destination account is required' })} placeholder='Destination account'>
-          <option value=''>Choose an account</option>
-          {accounts.map((account: IAccount) => (
-            <option value={account.accountId}>{account.username}</option>
-          ))}
-        </select>
-        <input type="number" placeholder='Value' {...register('value', { required: 'Values is required', min: 0.01 })} />
-        <button
-          type='submit'
-          onClick={onSubmit}
+      <Box
+        component="form"
+        sx={{
+          '& > :not(style)': {
+            m: 1,
+          },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <Stack
+        sx={{
+          width: { xs: '98%' },
+          display: 'flex',
+          flexDirection: {
+            md: 'row',
+            xs: 'column',
+          },
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
         >
-          Transfer
-        </button>
-      </form>
-      <span>{showMessage}</span>
-      <span>{errors.debitedAccountId?.message}</span>
-      <span>{errors.value?.message}</span>
-      <span>{errors.value?.type === 'min' && 'Value must be greater than 0.01'}</span>
-    </section>
+          <Typography
+          variant='h2'
+          sx={{
+            fontSize: '2rem',
+            color: 'text.primary',
+            textAlign: {
+              md: 'start',
+              xs: 'center',
+            },
+          }}
+          >
+            Transferir
+          </Typography>
+        <FormControl sx={{
+          minWidth: {
+            md: '110px',
+            xs: '300px',
+          },
+          mx: 2,
+          maxWidth: '300px',
+        }}>
+            <InputLabel id='orderByLabel'>Destino</InputLabel>
+            <Select
+              labelId='accountLabel'
+              id='accountId'
+              variant='outlined'
+              {...register('debitedAccountId', { required: 'Destination account is required' })}
+            >
+              {accounts.map((account: IAccount) => (
+                <MenuItem value={account.accountId}>{account.username}</MenuItem>
+              ))}
+            </Select>
+            <FormHelperText error>
+              {errors.debitedAccountId?.message}
+            </FormHelperText>
+          </FormControl>
+
+        <FormControl sx={{
+          minWidth: {
+            md: '110px',
+            xs: '300px',
+          },
+          maxWidth: '300px',
+        }}>
+            <OutlinedInput
+              type="number"
+              placeholder='Valor'
+              startAdornment={<InputAdornment position='start'>R$</InputAdornment>}
+              {...register('value', { required: 'Values is required', min: 0.01 })}
+            />
+            <FormHelperText error>
+              {errors.debitedAccountId?.message}
+            </FormHelperText>
+          </FormControl>
+          <Button
+            variant='outlined'
+            onClick={onSubmit}
+            sx={{ px: 2, mx: 2, width: { xs: '300px', md: '150px' } }}
+          >
+            Transferir
+          </Button>
+          <FormHelperText error>{showMessage}</FormHelperText>
+        </Stack>
+      </Box>
   );
 }
 
